412a9e2e4d545c8e7d1f60f874cf57978b25eb11
pravocodes <singhpraveen5673@gmail.com>
2023-07-15 12:33:03 +0530

    Client Folder

---
+++ client/src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layouts/Layout'
+
+const Contact = () => {
+  return (
+    <>
+    <Layout title = "Contact us"/>
+    </>
+  )
+}
+
+export default Contact
