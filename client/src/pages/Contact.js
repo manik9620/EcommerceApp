177bfa4efd36926db92c552ae40383220da5a64f
Praveen Kumar <114364779+pravocodes@users.noreply.github.com>
2023-07-20 15:44:48 +0530

    Revert "second commit"

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,30 +1,10 @@
 import React from 'react'
-import './Contact.css'
-// import Layout from '../components/Layouts/Layout'
-import Header from '../components/Layouts/Header'
-import Footer from '../components/Layouts/Footer'
+import Layout from '../components/Layouts/Layout'
+
 const Contact = () => {
   return (
     <>
-    <Header />
-    <div>
-	<div className="contact-container">
-		<div className="contact-box">
-			<div className="left"></div>
-			<div className="right">
-				<h2 className="contact-heading" style={{fontSize:'27px'}}>Contact Us</h2>
-				<input type="text" className="field" placeholder="Your Name" />
-				<input type="text" className="field" placeholder="Your Email" />
-				<input type="text" className="field" placeholder="Phone" />
-				<textarea placeholder="Message" className="field"></textarea>
-				<button className="btn">Send</button>
-			</div>
-
-		</div>
-		
-	</div>
-</div>
-<Footer/>
+    <Layout title = "Contact us"/>
     </>
   )
 }
